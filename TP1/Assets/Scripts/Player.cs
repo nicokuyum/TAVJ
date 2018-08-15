9e17f4aac3096674e44ce3d432cc57e0b30cf108
Nicolas Kuyumciyan <nkuyumciyan@medallia.com>
2018-08-15 19:11:54 -0300

    Initial commit

---
+++ TP1/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public int health;
+	public String destIp;
+	public int sourcePort;
+	public int destPort;
+
+	// Se llama luego de haber sido constuido el GameObject y todos sus componentes
+	void Awake () {
+		Debug.Log ("Health = " + health);
+	}
+
+	// Se llama antes del primer update (siempre despues de awake)
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			Debug.Log ("Sending UDP Packet to " + destIp + " port " + destPort);
+			SendUdp(sourcePort, destIp, destPort, Encoding.ASCII.GetBytes("Hello bitch"));
+		}
+	}
+	
+	static void SendUdp(int srcPort, string dstIp, int dstPort, byte[] data)
+	{
+		using (UdpClient c = new UdpClient(srcPort))
+			c.Send(data, data.Length, dstIp, dstPort);
+	}
+}
---
+++ TP1/Assets/Scripts/TCPClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+public class TCPClient : MonoBehaviour {
+	
+	private TcpClient connection; 
+
+	// Use this for initialization
+	void Start () {
+		Thread receive = new Thread (new ThreadStart(Listen)); 			
+		receive.IsBackground = true;		
+		receive.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void Listen()
+	{
+	
+	}
+}
